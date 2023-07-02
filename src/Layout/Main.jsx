ee4c26b962623c3ac937fb0176aafe919cb6f22e
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-07-02 16:25:08 +0600

    set Up Basic and create navber  bannar

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '../components/Container';
+import Home from '../Pages/Home/Home/Home';
+
+const Main = () => {
+    return (     
+             <Home></Home> 
+    );
+};
+
+export default Main;
